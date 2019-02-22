ec8e40d7c969dc175414ef90f880256d6531bcab
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-02-22 17:06:24 +0100

    Removed unused includes

--- src/bindings.cc
+++ src/bindings.cc
@@ -1,5 +1,4 @@
 #include "bindings.h"
-#include <opencv2/core/matx.hpp>
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <limits>
@@ -10,8 +9,6 @@ int cv_write(char const* path, unsigned char const* data, int rows, int cols, in
 	static_assert(std::numeric_limits<unsigned char>::digits == 8, "Unsuitable byte size");
 	
 	using cv::Mat;
-	using cv::Vec3b;
-	using cv::Point;
 	using cv::Size;
 
 	if(type != CV_8UC3) {
